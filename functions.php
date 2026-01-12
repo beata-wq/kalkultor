0689682d04406036d87ac9dfd8292879a2476915
beata-wq <miecznikbeata@gmail.com>
2026-01-12 14:47:50 +0100

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+
+$functions_folder = TEMPLATEPATH.'/functions/';
+require_once($functions_folder.'configs.php');
+require_once($functions_folder.'menus.php');
+require_once($functions_folder.'post-types.php');
